bcb110e531b95eed15ded61817a43b2f4d16da83
danielransom100 <danielransom100@hotmail.com>
2021-01-24 13:45:11 +0000

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+    < Text style={styles.text}>Welcome to Soccer Locker</Text>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 50
+  }
+});
